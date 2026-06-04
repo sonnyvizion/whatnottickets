0bd2b0fdb353b7164772f117fd9f2a050dfc1cea
Sonny Marcin <sonnvizion@gmail.com>
2026-06-04 09:50:49 +0200

    feat: mount CameraFlashes in root layout with direct client import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from "@/components/SmoothScroll";
-import CameraFlashesWrapper from "@/components/CameraFlashesWrapper";
+import CameraFlashes from "@/components/CameraFlashes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="fr" className={inter.variable}>
       <body>
         <SmoothScroll />
-        <CameraFlashesWrapper />
+        <CameraFlashes />
         <div style={{ position: "relative", zIndex: 1 }}>
           {children}
         </div>
--- src/components/CameraFlashesWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const CameraFlashes = dynamic(() => import("@/components/CameraFlashes"), { ssr: false });
-
-export default function CameraFlashesWrapper() {
-  return <CameraFlashes />;
-}
